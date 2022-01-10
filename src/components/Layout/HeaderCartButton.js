2a863708c2e76bf3e059ef54fce183af0005a646
Joshua Adu-Mensah (Qwaben Josh) <joshuaadu2001@gmail.com>
2022-01-10 22:16:55 +0000

    Initial commit

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,13 @@
+import classes from "./HeaderCartButton.module.css";
+const HeaderCartButton = (props) => {
+  return (
+    <button className={`${classes["header-cart-btn"]} ${props.className}`}>
+      <span>
+        <i class="fas fa-shopping-cart"></i>
+      </span>
+      Your Cart <span className={classes.total}>{props.total}</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
---
+++ src/components/UI/Logo.js
@@ -0,0 +1,6 @@
+import classes from "./Logo.module.css";
+const Logo = (props) => {
+  return <div className={classes.logo}>Betty Jewelry</div>;
+};
+
+export default Logo;
