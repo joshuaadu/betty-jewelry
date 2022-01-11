ff6f1106eea81d01d9185a2f10dd066d0b14f83f
Joshua Adu-Mensah (Qwaben Josh) <joshuaadu2001@gmail.com>
2022-01-11 20:07:38 +0000

    Add Meals Summary Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from "./components/Layout/Header";
+import MealsSummary from "./components/Meals/MealsSummary";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <main className="main">
+        <MealsSummary></MealsSummary>
+      </main>
     </div>
   );
 }
