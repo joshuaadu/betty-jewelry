64c7168127809a60d455bc36b69dd77f32437a11
Joshua Adu-Mensah (Qwaben Josh) <joshuaadu2001@gmail.com>
2022-01-24 19:15:09 +0000

    add a database for meals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,33 +1,7 @@
 import MealItem from "./MealItem";
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99
-  }
-];
+import DUMMY_MEALS from "./mealsDatabase";
 
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
