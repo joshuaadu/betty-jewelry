8388c7835d8c0cf5b342f63b7a0cddde6f4f6089
Joshua Adu-Mensah (Qwaben Josh) <joshuaadu2001@gmail.com>
2022-01-25 22:04:12 +0000

    Add animation to headerCartButton component

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,30 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 const HeaderCartButton = (props) => {
-  const cartCtx = useContext(CartContext);
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const { items } = useContext(CartContext);
+  const [btnHighlighted, setBtnHighLighted] = useState(false);
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnHighLighted(true);
+    const timer = setInterval(() => {
+      setBtnHighLighted(false);
+    }, 300);
+    return () => {
+      clearInterval(timer);
+    };
+  }, [items]);
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
+  const btnClass = `${classes["header-cart-btn"]} ${
+    btnHighlighted ? classes.bump : ""
+  }`;
   return (
     <button
-      className={`${classes["header-cart-btn"]} ${props.className}`}
+      className={`${btnClass} ${props.className}`}
       onClick={props.onClick}
     >
       <span>
