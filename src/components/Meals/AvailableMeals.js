67f2fd8c37f179eec652ba37f2c8543cbb9fa868
Joshua Adu-Mensah (Qwaben Josh) <joshuaadu2001@gmail.com>
2022-02-22 19:27:53 +0000

    fetch meals info from firebase database

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,18 +1,27 @@
 import MealItem from "./MealItem";
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
-import DUMMY_MEALS from "./mealsDatabase";
+import useSendRequest from "../hooks/use-sendRequest";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem
-      key={meal.id}
-      id={meal.id}
-      name={meal.name}
-      description={meal.description}
-      price={meal.price}
-    />
-  ));
+  const { data, isLoading, error } = useSendRequest();
+
+  const mealsList = isLoading ? (
+    <p>Loading meals info...</p>
+  ) : error ? (
+    <p>error</p>
+  ) : (
+    data.map((meal) => (
+      <MealItem
+        key={meal[0]}
+        id={meal[0]}
+        name={meal[1].name}
+        description={meal[1].description}
+        price={meal[1].price}
+      />
+    ))
+  );
+
   return (
     <section className={classes.meals}>
       <Card>
---
+++ src/components/hooks/use-sendRequest.js
@@ -0,0 +1,36 @@
+import { useCallback, useEffect, useState } from "react";
+
+const useSendRequest = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchMeals = useCallback(async (applydata) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        "https://react-form-fd387-default-rtdb.firebaseio.com/meals.json"
+      );
+      if (!response.ok) {
+        throw new Error("Unable to connect to database!");
+      }
+      const data = await response.json();
+      if (!data) {
+        throw new Error("Unable to receive response from database!");
+      }
+      setData(Object.entries(data));
+      setIsLoading(false);
+    } catch (error) {
+      setError(error.message);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchMeals();
+  }, [fetchMeals]);
+
+  return { data, isLoading, error };
+};
+
+export default useSendRequest;
