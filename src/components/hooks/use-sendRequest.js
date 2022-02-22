b81c851ecaa85ecd94d6b95072bb438545be8819
Joshua Adu-Mensah (Qwaben Josh) <joshuaadu2001@gmail.com>
2022-02-22 20:27:35 +0000

    fix firebase url

--- src/components/hooks/use-sendRequest.js
+++ src/components/hooks/use-sendRequest.js
@@ -10,7 +10,7 @@ const useSendRequest = () => {
     setError(null);
     try {
       const response = await fetch(
-        "https://react-fom-fd387-default-rtdb.firebaseio.com/meals.json"
+        "https://react-form-fd387-default-rtdb.firebaseio.com/meals.json"
       );
       if (!response.ok) {
         throw new Error("Unable to connect to database!");
